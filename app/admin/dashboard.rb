13bb446a54e4500df37baec94b4e74f9eb0758e7
Rômulo Danzer <romulodanzer@gmail.com>
2021-04-12 14:50:10 -0400

    okdashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,22 +4,20 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
   content :title => proc{ I18n.t("active_admin.dashboard") } do
 
-#    if Hidrante.ultimos_7_dias.count > 0
-#        panel "Dados Sesmt" do
-#            columns do
-#              column do
-#                panel "" do
-#                  para "Hello World"
-#                end
-#              end
-#              column do
-#                panel "" do
-#                  para "Hello World"
-#                end
-#              end
-#          end
-#        end
-#     end
+  
+    columns do
+      column do
+        panel "Registros Por Técnico" do
+          render "layouts/sesmt"
+        end
+      end
+      column do
+        panel "Registros Por Embarcação" do
+          render "layouts/sesmt2"
+        end
+      end
+    end
+  
 
   end
   end
